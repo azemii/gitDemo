7b49201ad60aa6e905d0c63d661ba45356ba984f
armend azemi <a.azemi90@gmail.com>
2017-04-06 00:00:04 +0200

    Added lastName

--- gitDemo/ViewController.swift
+++ gitDemo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     let userName = "New User Name"
     let userAge = 10
+    let lastName = "the name after the first"
     
     
     override func viewDidLoad() {
